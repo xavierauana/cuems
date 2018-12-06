550ae81e9542e9af52b72d330ebd87837c9094de
Xavier Au <xavier.au@anacreation.com>
2018-12-06 08:39:55 +0800

    transaction status change

--- app/Observers/TransactionObserver.php
+++ app/Observers/TransactionObserver.php
@@ -10,18 +10,25 @@ use App\Transaction;
 class TransactionObserver
 {
     public function saved(Transaction $transaction) {
+        if (!$transaction->isDirty('status')) {
+            return;
+        }
         switch ($transaction->status) {
             case TransactionStatus::COMPLETED:
-                event(new SystemEvent(SystemEvents::TRANSACTION_COMPLETED, $transaction));
+                event(new SystemEvent(SystemEvents::TRANSACTION_COMPLETED,
+                    $transaction));
                 break;
             case TransactionStatus::FAILED:
-                event(new SystemEvent(SystemEvents::TRANSACTION_FAILED, $transaction));
+                event(new SystemEvent(SystemEvents::TRANSACTION_FAILED,
+                    $transaction));
                 break;
             case TransactionStatus::REFUNDED:
-                event(new SystemEvent(SystemEvents::TRANSACTION_REFUND, $transaction));
+                event(new SystemEvent(SystemEvents::TRANSACTION_REFUND,
+                    $transaction));
                 break;
             case TransactionStatus::PROCESSING:
-                event(new SystemEvent(SystemEvents::TRANSACTION_PENDING, $transaction));
+                event(new SystemEvent(SystemEvents::TRANSACTION_PENDING,
+                    $transaction));
                 break;
             default:
                 break;
