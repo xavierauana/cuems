04760e14c9f4f15078174a084ace396753a8ef88
Xavier Au <xavier.au@anacreation.com>
2019-01-16 14:44:14 +0800

    update welcome page 4

--- app/Ticket.php
+++ app/Ticket.php
@@ -159,6 +159,8 @@ class Ticket extends Model
         $start_at = new Carbon($this->getOriginal['start_at']);
         $end_at = new Carbon($this->getOriginal['end_at']);
 
+        \Debugbar::info($this->getOriginal['start_at']);
+        \Debugbar::info($this->getOriginal['end_at']);
         \Debugbar::info($start_at);
         \Debugbar::info($end_at);
         \Debugbar::info($now->greaterThanOrEqualTo($start_at));
